a0b50a83553a80470652618477a84d0ef612b3b7
MeeraMeera1 <gatesameera98@gmail.com>
2020-12-16 00:22:41 -0500

    Produce now visible on app

--- src/components/ProduceList/ProduceList.js
+++ src/components/ProduceList/ProduceList.js
@@ -1,8 +1,23 @@
+/*
+  to display the produce mock data
+   we need the produce list component to access the produce slice of state 
+    is this basically like the component bought this and only this piece of pie?
+      think my understanding of componenets could be off
+*/
+
+/*
+useSelector - accepts a function as a parameter and will pass the updated state
+              into the function whenever the state gets updated
+*/
+
+import { useSelector } from 'react-redux';
 import ProduceDetails from './ProduceDetails';
 import './ProduceList.css';
 
+//the ProduceList component is turning the products into an array 
+  //the ProduceDetails componenet is then rendering that array
 function ProduceList() {
-  const produce = {};
+  const produce = useSelector(state => state.produce);
   
   const produceArr = Object.values(produce);
 
