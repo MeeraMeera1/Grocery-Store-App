e37638b3b67339cb23d46f5dfce898f8665d0817
MeeraMeera1 <gatesameera98@gmail.com>
2020-12-15 19:00:40 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+//import the Provider component from react-redux
+import { Provider } from 'react-redux';
+//importation of configureStore function into entry file
+import configureStore from './store';
 import './index.css';
 import App from './App';
 
+//initialize a variable called store and set it to configureStore 
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
 function Root() {
   return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    //wrap the BrowserRouter component with the Provider component
+    //Pass the prop of store with the value of store into the Provider 
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/store/index.js
@@ -0,0 +1,45 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+
+//createStore - creates a redux store (let's go shopping boo)
+
+//combineReducers - creates one reducer from multiple reducers as slices of state
+
+/*
+applyMiddleware - a store enhancer that will allow you to attach middlewares
+(a middleware is a function called before any action hits the root reducer)
+*/
+
+/*
+compose - another store enhancer that will allow you to use more than one store 
+enhancer
+*/
+
+const rootReducer = combineReducers ({
+
+});
+
+//used when app is in development
+let enhancer;
+
+if (process.env.NODE_ENV !== "production") {
+    //logger varaible set to the default export of the imported redux-logger package
+    //imported using require instead of ES6
+    const logger = require("redux-logger").default;
+    //composeEnhancers variable is set to the Redux dev tools extensions store enhancer
+    const composeEnhancers =
+    //this is a compose function from redux 
+    /*
+        the Redux compose function : takes in store enhancers as its arguments 
+        and combines them to create a single store ehancer.
+     */
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+        //applyMiddleware invoked with the loaded logger enhancer is set to the 
+        //combined store enchancer.
+    enhancer = composeEnhancers(applyMiddleware(logger));
+}
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
